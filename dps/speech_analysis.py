d68b1483a7c4dfc603c377e151553bb29c12c2fd
Jacob Hart <>
2024-06-12 10:14:40 +0200

    convert to ms

--- dps/speech_analysis.py
+++ dps/speech_analysis.py
@@ -53,11 +53,10 @@ class SpeechAnalysis:
         num_frames = math.floor(self.media_length / self.window_size)
         curve = np.zeros((num_frames, num_frames), dtype = int)
         for i, word in enumerate(self.raw_analysis):
-            start_frame = int(word["start"] / self.window_size)
-            end_frame = int(word["end"] / self.window_size)
+            start_frame = int(word["start"] * 1000 / self.window_size)
+            end_frame = int(word["end"] * 1000 / self.window_size)
             curve[start_frame:end_frame, 0] = 1
             curve[start_frame:end_frame, 1] = i + 1
-            print(f"{start_frame} - {end_frame} : 1 and {i + 1}...")
         return curve
     
     def display_raw_curve(self):
