11decd981c33fda25f392d250272b96c81db98ae
Jacob Hart <>
2024-06-12 10:21:22 +0200

    debug

--- dps/speech_analysis.py
+++ dps/speech_analysis.py
@@ -57,6 +57,7 @@ class SpeechAnalysis:
             end_frame = int(word["end"] * 1000 / self.window_size)
             curve[start_frame:end_frame, 0] = 1
             curve[start_frame:end_frame, 1] = i + 1
+            print(f"{start_frame}-{end_frame} : 1 and {i + 1}")
         return curve
     
     def display_raw_curve(self, dim = 0):
